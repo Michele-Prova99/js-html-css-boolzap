d7d49717548775cc41ea73c684668aa9aea8ac3b
Michele Prova <michele.prova99@gmail.com>
2020-07-30 12:38:54 +0200

    Layout completato (manca js)

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Milestone 2
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e,
+// cliccando invia (l'icona o anche con enter), il testo viene aggiunto al thread sopra,
+// come messaggio verde.
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
+// l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
